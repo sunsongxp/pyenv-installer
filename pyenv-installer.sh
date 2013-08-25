e63aca02dc7dfde5829345acbf95b95cbaa5a09c
song <sunsong@deliveryherochina.com>
2013-08-25 15:52:52 +0800

    Add swaks

--- pyenv-installer.sh
+++ pyenv-installer.sh
@@ -23,7 +23,7 @@ sudo sh <<SCRIPT
 
 
     # Basic Tools
-    apt-get -y install ssh vim tmux tilda ack-grep ipython traceroute tsocks
+    apt-get -y install ssh vim tmux tilda ack-grep ipython traceroute tsocks swaks
 
     # Version Control System
     apt-get -y install git subversion
