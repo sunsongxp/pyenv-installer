53269c8e2c26859d43cfce90adbd3eb655be10f8
song <sunsong@deliveryherochina.com>
2013-09-03 11:27:27 +0800

    Add django-debug-toolbar

--- pyenv-installer.sh
+++ pyenv-installer.sh
@@ -118,7 +118,7 @@ pip install django-geoip
 
 # psycopg2 is django PostgreSQL backend, html5lib can parse html5 file, html2text can convert html to markdown format
 
-pip install pep8 ipdb django-extensions
+pip install pep8 ipdb django-extensions django-debug-toolbar
 
 pip install requests
 pip install rq
