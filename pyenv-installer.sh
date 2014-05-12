684ab756cbe36e784aae0da63f65ca7fdf646a18
song <sunsongxp@gmail.com>
2014-05-12 15:58:09 +0800

    Add tig

--- pyenv-installer.sh
+++ pyenv-installer.sh
@@ -23,7 +23,7 @@ sudo sh <<SCRIPT
 
 
     # Basic Tools
-    apt-get -y install ssh openssh-server vim tmux tilda ack-grep ipython traceroute tsocks swaks curl ncdu pwgen
+    apt-get -y install ssh openssh-server vim tmux tilda ack-grep ipython traceroute tsocks swaks curl ncdu pwgen tig
 
     # Version Control System
     apt-get -y install git subversion
