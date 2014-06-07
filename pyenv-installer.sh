2cf28de4074ec6b81f5da934a1bea462e42094db
Sun Song <sunsongxp@gmail.com>
2014-06-07 15:03:53 +0800

    Add zsh

--- pyenv-installer.sh
+++ pyenv-installer.sh
@@ -23,7 +23,7 @@ sudo sh <<SCRIPT
 
 
     # Basic Tools
-    apt-get -y install ssh openssh-server tmux tilda ack-grep ipython traceroute tsocks swaks curl ncdu pwgen tig
+    apt-get -y install ssh openssh-server tmux tilda ack-grep ipython traceroute tsocks swaks curl ncdu pwgen tig zsh
 
     # Version Control System
     apt-get -y install git subversion
